fb3a6124f8cf623ff3119eb77a8a8c09df165888
Yann Bernard <yannbernard@macair.local>
2025-01-24 17:03:02 +0100

    config paths

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod configuration;
+
+use anyhow::Result;
 use chrono::NaiveDate;
 use configuration::{Config, TimeSlot};
+use std::env;
+use std::path::{Path, PathBuf};
 
-fn main() {
+fn main() -> Result<()> {
+    let path_to_config_dir: PathBuf = env::var("CRONPLANER_CONFIG_DIR")
+        .and_then(move |p| {
+            let n_p: PathBuf = p.into();
+            Ok(n_p)
+        })
+        .unwrap_or(Path::new("~").join(".config").join("cronplaner"));
     let date1 = NaiveDate::from_ymd_opt(2025, 02, 02).unwrap();
 
     let config = Config {
@@ -15,4 +25,7 @@ fn main() {
 
     let toml_t = toml::to_string(&config).unwrap();
     println!("Days: {:#?}", toml_t);
+    println!("Config Path: {:#?}", path_to_config_dir);
+
+    Ok(())
 }
