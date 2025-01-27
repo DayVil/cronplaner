4426cd3c70177c7fd44bb4114b33d5e25482bb77
Yann Bernard <yannbernard@macair.local>
2025-01-27 12:31:15 +0100

    config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,14 +1,41 @@
+use std::{env, path::Path};
+use std::{fs::File, io::Read, path::PathBuf};
+
+use anyhow::{Context, Result};
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Config {
-    pub id: i32,
-    pub time_slots: Vec<TimeSlot>,
-}
+static ENV_NAME: &str = "CRONPLANER_CONFIG_DIR";
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct TimeSlot {
     pub name: String,
     pub date: NaiveDate,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct TimeSlotsConfig {
+    pub time_slots: Vec<TimeSlot>,
+}
+
+impl TimeSlotsConfig {
+    pub fn new() -> Result<Self> {
+        #[cfg(feature = "example")]
+        env::set_var(ENV_NAME, "./example");
+
+        let path_to_config_dir: PathBuf = Self::get_config_dir()?;
+        let config_path = path_to_config_dir.join("appointments.toml");
+        let mut file = File::open(&config_path).context("Couldn't find appointments.toml")?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        let time_slots_config: TimeSlotsConfig = toml::from_str(&contents)?;
+
+        Ok(time_slots_config)
+    }
+
+    fn get_config_dir() -> Result<PathBuf> {
+        return Ok(env::var(ENV_NAME)
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| Path::new("~").join(".config").join("cronplaner")));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,19 @@ mod configuration;
 
 use anyhow::Result;
 use chrono::NaiveDate;
-use configuration::{Config, TimeSlot};
-use std::env;
-use std::path::{Path, PathBuf};
+use configuration::{TimeSlot, TimeSlotsConfig};
 
 fn main() -> Result<()> {
-    let path_to_config_dir: PathBuf = env::var("CRONPLANER_CONFIG_DIR")
-        .and_then(move |p| {
-            let n_p: PathBuf = p.into();
-            Ok(n_p)
-        })
-        .unwrap_or(Path::new("~").join(".config").join("cronplaner"));
-    let date1 = NaiveDate::from_ymd_opt(2025, 02, 02).unwrap();
+    let time_slots = TimeSlotsConfig::new()?;
 
-    let config = Config {
-        id: 12,
+    let d = TimeSlotsConfig {
         time_slots: vec![TimeSlot {
-            name: "One".into(),
-            date: date1,
+            name: "Hello".to_string(),
+            date: NaiveDate::from_ymd_opt(2002, 02, 23).unwrap(),
         }],
     };
-
-    let toml_t = toml::to_string(&config).unwrap();
-    println!("Days: {:#?}", toml_t);
-    println!("Config Path: {:#?}", path_to_config_dir);
+    println!("serial: {:?}", toml::to_string(&d));
+    println!("desirial: {:?}", time_slots);
 
     Ok(())
 }
